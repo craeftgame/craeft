7287dc6ad4a2db94e23a30d1bba9baac8f5c0c1a
Sebastian <13647606+H34D@users.noreply.github.com>
2025-09-15 21:40:39 +0200

    chore: refactor

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA)
-  basePath: "/",
+  basePath: "",
   images: {
     unoptimized: true,
   },
