67d19436d723f4ce7e7ad2ebd5182acd9910d479
Sebastian <13647606+H34D@users.noreply.github.com>
2025-10-27 08:20:24 +0100

    fix: viewport zooming
    Signed-off-by: Sebastian <13647606+H34D@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=2.0"
         />
 
         <meta
